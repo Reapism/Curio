1334c89f46215f294fdb5dcc031066291819176c
Anthony J <reapsprgm@gmail.com>
2021-03-06 01:03:21 -0500

    Update EmailSender to get rid of obselete SmptClient from System.Net.Mail and use MailKit
    - MailKit is a modern open source library for sending MIME (Multipurpose Internet Mail Extension) content, an IETF specification.
    - Left comments for TODO

--- Curio/src/Curio.Infrastructure/EmailSender.cs
+++ Curio/src/Curio.Infrastructure/EmailSender.cs
@@ -1,10 +1,15 @@
 ﻿using Curio.Core.Interfaces;
+using MailKit.Net.Smtp;
 using Microsoft.Extensions.Logging;
-using System.Net.Mail;
+using MimeKit;
 using System.Threading.Tasks;
 
 namespace Curio.Infrastructure
 {
+    // https://github.com/jstedfast/MailKit#using-mailkit
+    // TODO: Need to build Builders for emailing html templates easy.
+    // Maybe use dotliquid or build html for the emails.
+    // Determine if email client is smpt, pop3, or imap
     public class EmailSender : IEmailSender
     {
         private readonly ILogger<EmailSender> _logger;
@@ -16,18 +21,25 @@ namespace Curio.Infrastructure
 
         public async Task SendEmailAsync(string to, string from, string subject, string body)
         {
-            var emailClient = new SmtpClient("localhost");
-            var message = new MailMessage
+            await SendEmailAsyncInternal(to, from, subject, body);
+        }
+
+        private async Task SendEmailAsyncInternal(string to, string from, string subject, string body)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Default", from));
+            message.To.Add(new MailboxAddress("Hello", to));
+            message.Subject = subject;
+            message.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = body };
+
+
+            using (var client = new SmtpClient())
             {
-
-                From = new MailAddress(from),
-                Subject = subject,
-                Body = body
-
-
-            };
-            message.To.Add(new MailAddress(to));
-            await emailClient.SendMailAsync(message);
+                client.Connect("smpt.example.com", 587, false);
+                await client.AuthenticateAsync("username", "password");
+                await client.SendAsync(message);
+                client.Disconnect(true);
+            }
             _logger.LogWarning($"Sending email to {to} from {from} with subject {subject}.");
         }
     }
