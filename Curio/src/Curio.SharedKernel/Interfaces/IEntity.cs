5a7bc7a1d85a8288365b2a585f554830a0d020fa
Anthony J <reapsprgm@gmail.com>
2021-03-07 00:10:07 -0500

    Add IEntity as an interface to represent all entities

---
+++ Curio/src/Curio.SharedKernel/Interfaces/IEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Curio.SharedKernel.Interfaces
+{
+    /// <summary>
+    /// Any Entity must have an <see cref="Id"/>.
+    /// </summary>
+    public interface IEntity
+    {
+        /// <summary>
+        /// The unique identifier for an entity.
+        /// </summary>
+        Guid Id { get; set; }
+    }
+}
