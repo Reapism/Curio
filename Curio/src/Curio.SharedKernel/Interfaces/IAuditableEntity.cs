c267d952ae36f1ee06b15003ca2c872f43830951
Anthony J <reapsprgm@gmail.com>
2021-03-07 00:10:26 -0500

    Added IAuditableEntity which represents all auditable entities

---
+++ Curio/src/Curio.SharedKernel/Interfaces/IAuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Curio.SharedKernel.Interfaces
+{
+    public interface IAuditableEntity
+    {
+        DateTime LastModifiedDate { get; set; }
+        string LastModifiedByUser { get; set; }
+        DateTime CreatedDate { get; set; }
+        string CreatedByUser { get; set; }
+    }
+}
