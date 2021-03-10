26056c92443a3228626d322f2d44aaaad6f78f4b
Anthony J <reapsprgm@gmail.com>
2021-03-09 23:49:28 -0500

    Create Lookup Endpoint infrastructure

---
+++ Curio/src/Curio.Web/Endpoints/Base/LookupEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curio.Web.Endpoints.Base
+{
+    [ApiController]
+    public abstract class LookupEndpoint<TRequest, TResponse> : ControllerBase
+    {
+        [HttpGet]
+        public abstract ActionResult<TResponse> HandleLookupAsync();
+
+        [HttpGet]
+        public abstract ActionResult<TResponse> HandleLookupAsync(TRequest request);
+    }
+
+    [ApiController]
+    public abstract class LookupEndpointAsync<TRequest, TResponse> : ControllerBase
+    {
+        [HttpGet]
+        public abstract Task<ActionResult<TResponse>> HandleLookupAsync(
+            CancellationToken cancellationToken = default);
+
+        [HttpGet]
+        public abstract Task<ActionResult<TResponse>> HandleLookupAsync(
+            TRequest request,
+            CancellationToken cancellationToken = default);
+    }
+}
