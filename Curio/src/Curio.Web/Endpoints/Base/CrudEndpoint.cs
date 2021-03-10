989589f9eb560241260c67036b2a2b6a7db9437b
Anthony J <reapsprgm@gmail.com>
2021-03-09 23:49:11 -0500

    Implement CrudEndpoint infrastructure

---
+++ Curio/src/Curio.Web/Endpoints/Base/CrudEndpoint.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curio.Web.Endpoints.Base
+{
+    [ApiController]
+    public abstract class CrudEndpoint<TRequest, TResponse> : ControllerBase
+    {
+        [HttpPost]
+        public abstract ActionResult<TResponse> Create(TRequest request);
+
+        [HttpGet]
+        public abstract ActionResult<TResponse> Read(TRequest request);
+
+        [HttpPut]
+        public abstract ActionResult<TResponse> Update(TRequest request);
+
+        [HttpDelete]
+        public abstract ActionResult<TResponse> Delete(TRequest request);
+    }
+
+    [ApiController]
+    public abstract class CrudEndpointAsync<TRequest, TResponse> : ControllerBase
+    {
+        [HttpPost]
+        public abstract Task<ActionResult<TResponse>> CreateAsync(
+            TRequest request,
+            CancellationToken cancellationToken = default);
+
+        [HttpGet]
+        public abstract Task<ActionResult<TResponse>> ReadAsync(
+            TRequest request,
+            CancellationToken cancellationToken = default);
+
+        [HttpPut]
+        public abstract Task<ActionResult<TResponse>> UpdateAsync(
+            TRequest request,
+            CancellationToken cancellationToken = default);
+
+        [HttpDelete]
+        public abstract Task<ActionResult<TResponse>> DeleteAsync(
+            TRequest request,
+            CancellationToken cancellationToken = default);
+    }
+}
