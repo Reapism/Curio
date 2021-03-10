59b1dd601e45d7837075f027e110be3b2289ca14
Anthony J <reapsprgm@gmail.com>
2021-03-09 23:48:59 -0500

    Move BaseEndpoints into a single class for easier lookup

--- Curio/src/Curio.Web/Endpoints/Base/EndpointBase.cs
+++ Curio/src/Curio.Web/Endpoints/Base/EndpointBase.cs
@@ -5,16 +5,54 @@ using Microsoft.AspNetCore.Mvc;
 namespace Curio.Web.Endpoints.Base
 {
     /// <summary>
+    /// A class exposing actionable endpoints that can be used
+    /// for RESTful API services.
+    /// </summary>
+    /// <typeparam name="TRequest">The request type.</typeparam>
+    /// <typeparam name="TResponse">The response type</typeparam>
+    public static class EndpointBase
+    {
+        public static class WithRequest<TRequest>
+        {
+            public abstract class WithResponse<TResponse> : BaseEndpointSync
+            {
+                public abstract ActionResult<TResponse> Handle(TRequest request);
+            }
+
+            public abstract class WithoutResponse : BaseEndpointSync
+            {
+                public abstract ActionResult Handle(TRequest request);
+            }
+        }
+
+        public static class WithoutRequest
+        {
+            public abstract class WithResponse<TResponse> : BaseEndpointSync
+            {
+                public abstract ActionResult<TResponse> Handle();
+            }
+
+            public abstract class WithoutResponse : BaseEndpointSync
+            {
+                public abstract ActionResult Handle();
+            }
+        }
+    }
+
+    /// <summary>
+    /// A base class for all synchronous endpoints.
+    /// </summary>
+	[ApiController]
+    public abstract class BaseEndpointSync : ControllerBase
+    {
+    }
+
+    /// <summary>
     /// A base class for an endpoint that accepts parameters.
     /// </summary>
     /// <typeparam name="TRequest">The request type.</typeparam>
     /// <typeparam name="TResponse">The response type</typeparam>
-    /// <example>
-    /// public class Video : BaseAsyncEndpoint
-    ///     .WithRequest<RequestType>
-    ///     .WithResponse<ResponseType>
-    /// </example>
-    public static class BaseAsyncEndpoint
+    public static class EndpointAsyncBase
     {
         public static class WithRequest<TRequest>
         {
