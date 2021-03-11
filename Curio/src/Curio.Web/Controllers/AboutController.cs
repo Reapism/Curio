b6d132fff9332f40b3238c7ea016b931dfee46f3
Anthony J <reapsprgm@gmail.com>
2021-03-10 23:53:37 -0500

    Add about page

---
+++ Curio/src/Curio.Web/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curio.Web.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: AboutController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
