d1e50a13b3987bf65a8a0f5bc04532d7d2fe1655
Anthony J <reapsprgm@gmail.com>
2021-03-11 22:54:07 -0500

    cleanup

--- Curio/src/Curio.Infrastructure/Data/EfRepository.cs
+++ Curio/src/Curio.Infrastructure/Data/EfRepository.cs
@@ -11,51 +11,51 @@ namespace Curio.Infrastructure.Data
 {
     public class EfRepository : IRepository
     {
-        private readonly CurioClientDbContext _dbContext;
+        private readonly CurioClientDbContext dbContext;
 
         public EfRepository(CurioClientDbContext dbContext)
         {
-            _dbContext = dbContext;
+            this.dbContext = dbContext;
         }
 
         public T GetById<T>(Guid id) where T : BaseEntity
         {
-            return _dbContext.Set<T>().SingleOrDefault(e => e.Id == id);
+            return dbContext.Set<T>().SingleOrDefault(e => e.Id == id);
         }
 
         public Task<T> GetByIdAsync<T>(Guid id) where T : BaseEntity
         {
-            return _dbContext.Set<T>().SingleOrDefaultAsync(e => e.Id == id);
+            return dbContext.Set<T>().SingleOrDefaultAsync(e => e.Id == id);
         }
 
         public Task<List<T>> ListAsync<T>() where T : BaseEntity
         {
-            return _dbContext.Set<T>().ToListAsync();
+            return dbContext.Set<T>().ToListAsync();
         }
 
         public Task<List<T>> ListAsync<T>(Expression<Func<T, bool>> predicate) where T : BaseEntity
         {
-            return _dbContext.Set<T>().Where(predicate).ToListAsync();
+            return dbContext.Set<T>().Where(predicate).ToListAsync();
         }
 
         public async Task<T> AddAsync<T>(T entity) where T : BaseEntity
         {
-            await _dbContext.Set<T>().AddAsync(entity);
-            await _dbContext.SaveChangesAsync();
+            await dbContext.Set<T>().AddAsync(entity);
+            await dbContext.SaveChangesAsync();
 
             return entity;
         }
 
         public async Task UpdateAsync<T>(T entity) where T : BaseEntity
         {
-            _dbContext.Entry(entity).State = EntityState.Modified;
-            await _dbContext.SaveChangesAsync();
+            dbContext.Entry(entity).State = EntityState.Modified;
+            await dbContext.SaveChangesAsync();
         }
 
         public async Task DeleteAsync<T>(T entity) where T : BaseEntity
         {
-            _dbContext.Set<T>().Remove(entity);
-            await _dbContext.SaveChangesAsync();
+            dbContext.Set<T>().Remove(entity);
+            await dbContext.SaveChangesAsync();
         }
     }
 }
