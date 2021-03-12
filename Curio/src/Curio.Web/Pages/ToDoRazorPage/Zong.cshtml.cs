906ef78fbeafcf76d85c98810815d05f90395880
Anthony J <reapsprgm@gmail.com>
2021-03-11 22:28:56 -0500

    Add new test RazorPage
    - RazorPages come with a backing model.

---
+++ Curio/src/Curio.Web/Pages/ToDoRazorPage/Zong.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Curio.Core.Entities;
+using Curio.SharedKernel.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Curio.Web.Pages.ToDoRazorPage
+{
+    public class ZongModel : PageModel
+    {
+        private readonly IRepository repository;
+
+        private IEnumerable<ToDoItem> Items { get; set; }
+
+        public ZongModel(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task OnGetAsync()
+        {
+            Items = await repository.ListAsync<ToDoItem>();
+        }
+    }
+}
