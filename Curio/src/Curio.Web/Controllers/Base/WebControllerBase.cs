2ea9fb9994d06468d6018aeb49a74f0616a5e055
Anthony J <reapsprgm@gmail.com>
2021-03-14 23:51:29 -0400

    (Repository NOTE) Creating a new Base WebController to include for IRepositories for free just from inheriting them
    - Need to discuss different types of IRepositories that generic at interface level, or method level.
    - - Method level generics allows use of any T that is meets the contraints of the method
    - - - i.e. IRepository (non generic) can query any types since using it is repository.Get<TAny>(id); where TAny
    - - Interface level generics only allows the use of the type specified in the declaration.
    - TODO think about composition in the infrastructure base classes and if its more beneficial then inheritence
    - Create different types of repository interfaces that are generic and secured
    
    - - IAsyncRepository<TEntity>, IRepository<TEntity>, ISecuredRepository<TModel>
    
    - API may only use this generic interfaces to prevent anyway for these services to query other types.
    - Web application might be able to query these types
    - IRepository at the moment allows any BaseEntity to be used for query.
    - - I may want to create another base class to allow only certain entities to be used to query data entities globally in this sense from a single service.

---
+++ Curio/src/Curio.Web/Controllers/Base/WebControllerBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Curio.SharedKernel.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curio.Web.Controllers.Base
+{
+    public class WebControllerBase : Controller
+    {
+        public WebControllerBase(IRepository repository)
+        {
+            Repository = repository;
+        }
+
+        protected IRepository Repository { get; }
+    }
+}
