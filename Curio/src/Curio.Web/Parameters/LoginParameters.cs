b666c86652e3a7b0f427d277f5e5a508d6e342fa
Anthony J <reapsprgm@gmail.com>
2021-03-14 23:41:05 -0400

    LoginParameters DTO class that will be moved to Core eventually

---
+++ Curio/src/Curio.Web/Parameters/LoginParameters.cs
@@ -0,0 +1,9 @@
+﻿namespace Curio.Web.Parameters
+{
+    public class LoginParameters
+    {
+        public string LoginName { get; set; }
+        public string Password { get; set; }
+        public bool IsEmailLogin { get; set; }
+    }
+}
