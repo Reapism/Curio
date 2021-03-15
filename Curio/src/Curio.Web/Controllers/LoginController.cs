380a352159302271b44cff6a358b75a057dadfa5
Anthony J <reapsprgm@gmail.com>
2021-03-14 23:54:13 -0400

    Add login and registration controller (stubs)

---
+++ Curio/src/Curio.Web/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Curio.SharedKernel.Interfaces;
+using Curio.Web.Controllers.Base;
+using Curio.Web.Parameters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curio.Web.Controllers
+{
+    public class LoginController : WebControllerBase
+    {
+        public LoginController(IRepository repository) : base(repository)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginParameters loginParameters)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Curio/src/Curio.Web/Controllers/RegistrationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Curio.SharedKernel.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curio.Web.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private readonly IRepository repository;
+
+        public RegistrationController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+    }
+}
