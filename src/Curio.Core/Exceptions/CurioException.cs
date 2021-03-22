9500a6229fd2ea2e74dabd7706f7ad45ff8436bc
Anthony J <reapsprgm@gmail.com>
2021-03-22 01:09:14 -0400

    Create new base exception `CurioException`

---
+++ src/Curio.Core/Exceptions/CurioException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Curio.Core.Exceptions
+{
+    /// <summary>
+    /// The base class for all Curio
+    /// exceptions.
+    /// </summary>
+    public class CurioException : Exception
+    {
+        public CurioException(string message)
+            : base (message)
+        {
+        }
+        
+        public CurioException Flatten()
+        {
+            var exceptions = new Queue<Exception>();
+
+            while (!(this.InnerException is null))
+                exceptions.Enqueue(this.InnerException);
+
+            var delimitedMessage = string.Join(Environment.NewLine, exceptions.Select(e => e.Message));
+            var delimitedStackTrace = string.Join($"{Environment.NewLine}{Environment.NewLine}", exceptions.Select(e => e.StackTrace));
+            var curioException = new CurioException(delimitedMessage);
+
+            return curioException;
+
+        }
+    }
+}
