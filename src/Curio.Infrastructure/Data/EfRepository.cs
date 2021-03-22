28123e9d395d606e71692bf67182754dcaf37bfc
Anthony J <reapsprgm@gmail.com>
2021-03-22 01:08:10 -0400

    Add sync repository functions

--- src/Curio.Infrastructure/Data/EfRepository.cs
+++ src/Curio.Infrastructure/Data/EfRepository.cs
@@ -18,6 +18,26 @@ namespace Curio.Infrastructure.Data
             this.dbContext = dbContext;
         }
 
+        public T Add<T>(T entity) where T : BaseEntity
+        {
+            dbContext.Set<T>().Add(entity);
+            dbContext.SaveChanges();
+
+            return entity;
+        }
+
+        public void Update<T>(T entity) where T : BaseEntity
+        {
+            dbContext.Entry(entity).State = EntityState.Modified;
+            dbContext.SaveChanges();
+        }
+
+        public void Delete<T>(T entity) where T : BaseEntity
+        {
+            dbContext.Set<T>().Remove(entity);
+            dbContext.SaveChanges();
+        }
+
         public T GetById<T>(Guid id) where T : BaseEntity
         {
             return dbContext.Set<T>().SingleOrDefault(e => e.Id == id);
@@ -28,14 +48,32 @@ namespace Curio.Infrastructure.Data
             return dbContext.Set<T>().SingleOrDefaultAsync(e => e.Id == id);
         }
 
+        public List<T> List<T>() where T : BaseEntity
+        {
+            return dbContext.Set<T>().ToList();
+        }
+
         public Task<List<T>> ListAsync<T>() where T : BaseEntity
         {
             return dbContext.Set<T>().ToListAsync();
         }
 
+        public List<T> List<T>(Expression<Func<T, bool>> predicate) where T : BaseEntity
+        {
+            return dbContext.Set<T>().Where(predicate).ToList();
+        }
+
         public Task<List<T>> ListAsync<T>(Expression<Func<T, bool>> predicate) where T : BaseEntity
         {
             return dbContext.Set<T>().Where(predicate).ToListAsync();
+        }
+
+        public IEnumerable<T> Add<T>(IEnumerable<T> entities) where T : BaseEntity
+        {
+            dbContext.Set<T>().AddRange(entities);
+            dbContext.SaveChanges();
+
+            return entities;
         }
 
         public async Task<T> AddAsync<T>(T entity) where T : BaseEntity
@@ -44,6 +82,14 @@ namespace Curio.Infrastructure.Data
             await dbContext.SaveChangesAsync();
 
             return entity;
+        }
+
+        public async Task<IEnumerable<T>> AddAsync<T>(IEnumerable<T> entities) where T : BaseEntity
+        {
+            await dbContext.Set<T>().AddRangeAsync(entities);
+            await dbContext.SaveChangesAsync();
+
+            return entities;
         }
 
         public async Task UpdateAsync<T>(T entity) where T : BaseEntity
--- src/Curio.SharedKernel/Interfaces/IRepository.cs
+++ src/Curio.SharedKernel/Interfaces/IRepository.cs
@@ -13,10 +13,19 @@ namespace Curio.SharedKernel.Interfaces
     /// </summary>
     public interface IRepository
     {
+        T GetById<T>(Guid id) where T : BaseEntity;
+        List<T> List<T>() where T : BaseEntity;
+        List<T> List<T>(Expression<Func<T, bool>> predicate) where T : BaseEntity;
+        T Add<T>(T entity) where T : BaseEntity;
+        IEnumerable<T> Add<T>(IEnumerable<T> entities) where T : BaseEntity;
+        void Update<T>(T entity) where T : BaseEntity;
+        void Delete<T>(T entity) where T : BaseEntity;
+
         Task<T> GetByIdAsync<T>(Guid id) where T : BaseEntity;
         Task<List<T>> ListAsync<T>() where T : BaseEntity;
         Task<List<T>> ListAsync<T>(Expression<Func<T, bool>> predicate) where T : BaseEntity;
         Task<T> AddAsync<T>(T entity) where T : BaseEntity;
+        Task<IEnumerable<T>> AddAsync<T>(IEnumerable<T> entities) where T : BaseEntity;
         Task UpdateAsync<T>(T entity) where T : BaseEntity;
         Task DeleteAsync<T>(T entity) where T : BaseEntity;
     }
@@ -24,6 +33,13 @@ namespace Curio.SharedKernel.Interfaces
     public interface IRepository<T>
         where T : BaseEntity
     {
+        T GetById(Guid id);
+        List<T> List();
+        List<T> List(Expression<Func<T, bool>> predicate);
+        T Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
         Task<T> GetByIdAsync(Guid id);
         Task<List<T>> ListAsync();
         Task<List<T>> ListAsync(Expression<Func<T, bool>> predicate);
