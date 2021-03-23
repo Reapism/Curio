00f27d610ed4df30b345b98d6615daca36ef8fad
Anthony J <reapsprgm@gmail.com>
2021-03-22 23:51:47 -0400

    Add UserPostService

---
+++ src/Curio.Core/Services/UserPostService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Curio.SharedKernel.Interfaces;
+
+namespace Curio.Core.Services
+{
+    public interface IUserPostService
+    {
+        
+    }
+
+    public class UserPostService : IUserPostService
+    {
+        private readonly IRepository repository;
+
+        public UserPostService(IRepository repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
