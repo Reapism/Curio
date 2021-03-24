192ccac74aef6985cb0c6b426d6e86e4c44b7313
Anthony J <reapsprgm@gmail.com>
2021-03-23 21:17:11 -0400

    Remove parameters class

--- src/Curio.Web/Parameters/LoginParameters.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Curio.Web.Parameters
-{
-    public class LoginParameters
-    {
-        public string LoginName { get; set; }
-        public string Password { get; set; }
-        public bool IsEmailLogin { get; set; }
-    }
-}
