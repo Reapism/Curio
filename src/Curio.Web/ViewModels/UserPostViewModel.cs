4c5bfdebed758c7e973cf79083c5827092a79e15
Anthony J <reapsprgm@gmail.com>
2021-03-24 23:31:39 -0400

    Creating a view model stub for a user post

---
+++ src/Curio.Web/ViewModels/UserPostViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Curio.Core.Entities;
+using Curio.Core.Enums;
+
+namespace Curio.Web.ViewModels
+{
+    public class UserPostViewModel
+    {
+        /// <summary>
+        /// Use <see cref="FromUserPost(UserPost)"/>
+        /// </summary>
+        private UserPostViewModel()
+        {
+
+        }
+        
+        public string Contents { get; set; }
+        public byte[] AuthorAvatar { get; set; }
+        public DateTime DateCreated { get; set; }
+        public bool IsAuthorVerified { get; set; }
+        public int NumberOfReplies { get; set; }
+        public int NumberOfReposts { get; set; }
+        public int NumberOfLikes { get; set; }
+        public UserPostSourceType UserPostSource { get; set; }
+        public IEnumerable<UserPostReplyViewModel> Replies { get; set; } = Enumerable.Empty<UserPostReplyViewModel>();
+
+        public static UserPostViewModel FromUserPost(UserPost userPost)
+        {
+            return new UserPostViewModel()
+            {
+                Title = userPost.Title
+            };
+        }
+    }
+
+    public class UserPostReplyViewModel
+    {
+
+    }
+}
