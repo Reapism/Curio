e361be4aa8cf66254d1e9409de12ad7a38a03ccc
Anthony J <reapsprgm@gmail.com>
2021-03-24 23:31:27 -0400

    Add new UserPostSourceType enum to describe where the post source platform is from

---
+++ src/Curio.Core/Enums/UserPostSourceType.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace Curio.Core.Enums
+{
+    public enum UserPostSourceType
+    {
+        [Description("Curio Web")]
+        WebApp,
+        [Description("Curio Android")]
+        Android,
+        [Description("Curio iPhone")]
+        IPhone,
+        [Description("Curio API")]
+        CurioApi,
+        [Description("Promoted")]
+        Advertisment
+    }
+}
