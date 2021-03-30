a73d8f3ca55c878a68f5a073dcaad82049b3b70e
Anthony J <reapsprgm@gmail.com>
2021-03-29 23:48:15 -0400

    Add Authentication/Authorization middleware to ASP.NET Core pipeline

--- src/Curio.Web/Startup.cs
+++ src/Curio.Web/Startup.cs
@@ -91,6 +91,9 @@ namespace Curio.Web
             app.UseSwagger();
             app.UseSession();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
 
