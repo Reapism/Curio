99f02a30a1c15c1198ebdb327bc750a2abd25c8a
Anthony J <reapsprgm@gmail.com>
2021-04-03 22:35:24 -0400

    Init RegisterUserService

---
+++ src/Curio.Core/Services/RegisterUserService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Curio.Core.Models;
+using Curio.SharedKernel.Interfaces;
+
+namespace Curio.Core.Services
+{
+    public class RegisterUserService
+    {
+        private readonly IRepository repository;
+
+        public RegisterUserService(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<ApiResponse<RegistrationResponse>> RegisterUser(RegistrationModel registrationModel)
+        {
+            
+        }
+
+        private bool HasRegistered(string email)
+        {
+            var user = repository.
+        }
+    }
+}
