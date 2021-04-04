76359afb6a76bdd16cfa7247519d49cb1cc801be
Anthony J <reapsprgm@gmail.com>
2021-04-03 22:35:12 -0400

    Registration model required for registering users

---
+++ src/Curio.Core/Models/RegistrationModel.cs
@@ -0,0 +1,19 @@
+﻿using Curio.SharedKernel.Interfaces;
+
+namespace Curio.Core.Models
+{
+    public class RegistrationModel : IModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MobilePhone { get; set; }
+        public string DisplayName { get; set; }
+    }
+
+    public class RegistrationResponse : IModel
+    {
+
+    }
+}
