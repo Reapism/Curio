40040c094b90dba3170fec268ef83aa7c45a30f9
Anthony J <reapsprgm@gmail.com>
2021-04-03 22:33:37 -0400

    Add new action filter to null Exception property on ApiResponses

---
+++ src/Curio.Web/Filters/HandleApiResponse.cs
@@ -0,0 +1,29 @@
+﻿using Curio.SharedKernel.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Curio.Web.Filters
+{
+    public class HandleApiResponse : ActionFilterAttribute
+    {
+        private readonly IWebHostEnvironment environment;
+
+        public HandleApiResponse(IWebHostEnvironment environment)
+        {
+            this.environment = environment;
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (environment.EnvironmentName == "Production")
+            {
+                var isApiResponse = context.Result is IApiResponse apiResponse;
+                if (isApiResponse)
+                {
+                    apiResponse = (IApiResponse)context.Result;
+                    apiResponse.Exception = null;
+                }
+            }
+        }
+    }
+}
