0cea65bbc7534f521d8fa3878cdbc9e4cf93f380
Anthony J <reapsprgm@gmail.com>
2021-04-05 23:10:33 -0400

    useful Json extensions for all generic types

---
+++ src/Curio.Core/Extensions/JsonExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace Curio.Core.Extensions
+{
+    public static class JsonExtensions
+    {
+        private static readonly JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        public static T FromJson<T>(this string json) =>
+            JsonSerializer.Deserialize<T>(json, options);
+
+        public static string ToJson<T>(this T obj) =>
+            JsonSerializer.Serialize<T>(obj, options);
+    }
+}
