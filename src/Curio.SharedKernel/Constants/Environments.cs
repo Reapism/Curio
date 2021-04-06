e28cf87647e14fcd21fdf2c526cf44b197234cab
Anthony J <reapsprgm@gmail.com>
2021-04-05 22:36:12 -0400

    Create Environments constants

---
+++ src/Curio.SharedKernel/Constants/Environments.cs
@@ -0,0 +1,13 @@
+﻿namespace Curio.SharedKernel.Constants
+{
+    public class EnvironmentConstants
+    {
+        public const string Development = nameof(Development);
+        ///<summary>Staging environment before code ships normally to production</summary>
+        public const string Staging = nameof(Staging);
+        ///<summary>Beta environment before code ships normally to production
+        /// which is made available to some users</summary>
+        public const string Beta = nameof(Beta);
+        public const string Production = nameof(Production);
+    }
+}
