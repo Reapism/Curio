202d71b21e5e0f2b239a05d2efaa5eb99ad6b6cc
Anthony J <reapsprgm@gmail.com>
2021-04-05 22:13:20 -0400

    Ability to mark certain entities as aggregate roots
        /// <summary>
        /// Marks a particular entity as being the aggregate root.
        /// <para>This entity will have no requirements, can be
        /// created regardless of independent of other aggregates.</para>
        /// </summary>

---
+++ src/Curio.Core/Interfaces/IAggregateRoot.cs
@@ -0,0 +1,11 @@
+﻿namespace Curio.Core.Interfaces
+{
+    /// <summary>
+    /// Marks a particular entity as being the aggregate root.
+    /// <para>This entity will have no requirements, can be 
+    /// created regardless of independent of other aggregates.</para>
+    /// </summary>
+    public interface IAggregateRoot
+    {
+    }
+}
