2b276b7f38d658b586186fbba18fb2f3592a5ef8
Anthony J <reapsprgm@gmail.com>
2021-04-08 00:15:10 -0400

    Add ValidationResponse and Tuple extension for creating a dictionary

---
+++ src/Curio.SharedKernel/Bases/ValidationResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Curio.SharedKernel.Interfaces;
+
+namespace Curio.SharedKernel.Bases
+{
+    public abstract class ValidationResponse : IValidationResponse
+    {
+        public bool IsFailure { get; set; }
+        public IDictionary<string, string> ValidationToTipMapping { get; set; } = new Dictionary<string, string>();
+    }
+}
---
+++ src/Curio.SharedKernel/Extensions/IValidationResponseExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Curio.SharedKernel.Bases;
+using Curio.SharedKernel.Interfaces;
+
+namespace Curio.SharedKernel.Extensions
+{
+    public static class IValidationResponseExtensions
+    {
+        public static IDictionary<string, string> ToValidationResponse(this (string, string) validationTipTuple)
+        {
+            return new Dictionary<string, string>()
+            {
+                { validationTipTuple.Item1, validationTipTuple.Item2 }
+            };
+        }
+    }
+}
