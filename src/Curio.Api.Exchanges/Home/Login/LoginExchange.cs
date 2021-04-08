f0be5d28baa4e08a425c01a7eab18f00cc135524
Anthony J <reapsprgm@gmail.com>
2021-04-07 23:05:53 -0400

    LoginExchange

---
+++ src/Curio.Api.Exchanges/Home/Login/LoginExchange.cs
@@ -0,0 +1,14 @@
+﻿namespace Curio.Api.Exchanges.Home.Login
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginResponse
+    {
+        public bool IsFailure { get; set; }
+        public string FailureMessage { get; set; }
+    }
+}
