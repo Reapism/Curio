3d82fe330c478911f75c172794d8e95e3f48709d
Anthony J <reapsprgm@gmail.com>
2021-04-08 00:17:12 -0400

    Possible implementation of the Validation class
    - Dictionary may contain this as a key.

---
+++ src/Curio.SharedKernel/Interfaces/Validation.cs
@@ -0,0 +1,16 @@
+﻿namespace Curio.SharedKernel.Interfaces
+{
+    public class Validation
+    {
+        public bool IsValid { get; set; }
+        public string Message { get; set; }
+        public ValidationType ValidationType { get; set; }
+    }
+
+    public enum ValidationType
+    {
+        Warning,
+        Failure,
+
+    }
+}
