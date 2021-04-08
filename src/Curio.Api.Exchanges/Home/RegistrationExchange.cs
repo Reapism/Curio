2ea75674a1009bfcce748e4db4bb9522dad29556
Anthony J <reapsprgm@gmail.com>
2021-04-08 00:16:29 -0400

    Init for RegistrationExchange

---
+++ src/Curio.Api.Exchanges/Home/RegistrationExchange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Curio.Api.Exchanges.Bases;
+using Curio.SharedKernel.Bases;
+
+namespace Curio.Api.Exchanges.Home
+{
+    public class RegistrationRequest
+    {
+        // Login details
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string MobilePhone { get; set; }
+
+        // Your information
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Profile 
+        public string DisplayName { get; set; }
+        public byte[] ImageBase64 { get; set; }
+    }
+
+    public class RegistrationResponse : ValidationResponse
+    {
+
+    }
+}
