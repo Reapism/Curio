4bdea145034f35eb71f23e94daac046e3cbca4d8
Anthony J <reapsprgm@gmail.com>
2021-04-07 23:52:45 -0400

    Create IValidationResponse

---
+++ src/Curio.Api.Exchanges/Bases/IValidationResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Curio.Api.Exchanges.Bases
+{
+    public interface IValidationResponse
+    {
+        bool IsFailure { get; set; }
+        IDictionary<string, string> ValidationToTipMapping { get; set; } // "Cannot contain spaces", "Try to not use spaces"
+
+    }
+}
