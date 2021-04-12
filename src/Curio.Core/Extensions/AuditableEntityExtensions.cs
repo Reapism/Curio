f3441205f3668cc86d1a0a67b3eb6ffc017befa0
Anthony J <reapsprgm@gmail.com>
2021-04-11 23:47:08 -0400

    Add new AuditableExtensions for assisting in creating/modifying audit values.

---
+++ src/Curio.Core/Extensions/AuditableEntityExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Curio.SharedKernel.Bases;
+
+namespace Curio.Core.Extensions
+{
+    public static class AuditableEntityExtensions
+    {
+        public static T NewAuditableEntity<T>(this T auditableEntity)
+            where T : AuditableEntity
+        {
+            auditableEntity.CreatedDate = DateTime.Now;
+            auditableEntity.LastModifiedDate = DateTime.Now;
+            auditableEntity.CreatedByUser = "SYSTEM";
+            auditableEntity.LastModifiedByUser = "SYSTEM";
+
+            return auditableEntity;
+        }
+
+        public static T UpdateAuditableEntity<T>(this T auditableEntity)
+            where T : AuditableEntity
+        {
+            auditableEntity.LastModifiedDate = DateTime.Now;
+            auditableEntity.LastModifiedByUser = "SYSTEM";
+
+            return auditableEntity;
+        }
+    }
+}
