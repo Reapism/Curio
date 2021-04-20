9a5dc550f1c5e78f4e8c4d676c6a77201a7b5319
Anthony J <reapsprgm@gmail.com>
2021-04-20 17:12:33 -0400

    Create some failing tests for EmailValidator

---
+++ tests/Curio.UnitTests/Infrastructure/Services/EmailValidatorTests.cs
@@ -0,0 +1,25 @@
+﻿using Curio.Infrastructure.Services;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Curio.UnitTests.Infrastructure.Services
+{
+    [TestFixture]
+    public class EmailValidatorTests
+    {
+        [TestCase(null, null)]
+        [TestCase("", "@")]
+        [TestCase("", "")]
+        [TestCase("hi", "hi")]
+        [TestCase("hi", "hi@email.com")]
+        [TestCase("hi", "hi@hi.com")]
+        [TestCase("hi", "hi@")]
+        [TestCase("", "@@")]
+        [TestCase("", "@.@")]
+        public void GetEmailNameFromEmail_ShouldReturnEmailName(string expected, string actual)
+        {
+            var validator = new EmailValidator();
+            validator.GetEmailName(actual).Should().BeSameAs(expected);
+        }
+    }
+}
