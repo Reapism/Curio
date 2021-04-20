4205d81d959bc91381328fb3240817df486ec0c3
Anthony J <reapsprgm@gmail.com>
2021-04-20 17:14:17 -0400

    FileResponse

---
+++ src/Curio.SharedKernel/Interfaces/IFileResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Curio.SharedKernel.Interfaces
+{
+    public interface IFileResponse
+    {
+        string FileName { get; set; }
+        bool IsEmpty { get; set; }
+        byte[] Data { get; set; }
+    }
+}
