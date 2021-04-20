4804dc49fa463980e79a5d801ad25e6092306c3a
Anthony J <reapsprgm@gmail.com>
2021-04-20 17:12:06 -0400

    Begin work on IEmailBuilder

---
+++ src/Curio.Core/Interfaces/IEmailBuilder.cs
@@ -0,0 +1,16 @@
+﻿namespace Curio.Core.Interfaces
+{
+    public interface IEmailBuilder
+    {
+        IEmailBuilder AddFromAddress(params string[] fromAddresses);
+        IEmailBuilder AddToAddress(params string[] toAddresses);
+        IEmailBuilder AddBccAddress(params string[] bccAddresses);
+        IEmailBuilder AddCcAddress(params string[] ccAddresses);
+        IEmailBuilder AddAttachment();
+
+        IEmailBuilder SetSubject(string subject);
+        IEmailBuilder SetBody(string content);
+
+        IEmailBuilder Build();
+    }
+}
---
+++ src/Curio.Infrastructure/Services/EmailBuilder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Curio.Core.Interfaces;
+using MimeKit;
+
+namespace Curio.Infrastructure.Services
+{
+    public class EmailBuilder : IEmailBuilder
+    {
+        private MimeMessage mimeMessage;
+        public EmailBuilder()
+        {
+            mimeMessage = new MimeMessage();
+        }
+
+        public IEmailBuilder AddAttachment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEmailBuilder AddBccAddress(params string[] bccAddresses)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEmailBuilder AddCcAddress(params string[] ccAddresses)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEmailBuilder AddFromAddress(params string[] fromAddresses)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEmailBuilder AddToAddress(params string[] toAddresses)
+        {
+            var internetAddresses = new Queue<InternetAddress>();
+            foreach (var toAddress in toAddresses)
+            {
+
+                internetAddresses.Enqueue(new MailboxAddress(Encoding.UTF8, "", toAddress));
+            }
+
+            mimeMessage.To.AddRange(internetAddresses);
+            return this;
+        }
+
+        public IEmailBuilder Build()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEmailBuilder SetBody(string content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEmailBuilder SetSubject(string subject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
