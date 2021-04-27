1715fa427beae8b1f5d5786dafc6b85f70d01d7c
Anthony J <reapsprgm@gmail.com>
2021-04-27 17:22:14 -0400

    Guard changes

--- src/Curio.Core/Extensions/IValidationResponseExtensions.cs
+++ src/Curio.Core/Extensions/IValidationResponseExtensions.cs
@@ -1,9 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Curio.SharedKernel.Interfaces;
 
 namespace Curio.Core.Extensions
 {
     public static class IValidationResponseExtensions
     {
+        public static Tuple<string, string> First(this IValidationResponse validationResponse)
+        {
+            if (validationResponse.ValidationToTipMapping.Count > 0)
+            {
+                return validationResponse.ValidationToTipMapping.First().AsTuple();
+            }
+
+            return Tuple.Create<string, string>(null, null);
+        }
+
         public static IDictionary<string, string> ToValidationResponse(this (string, string) validationTipTuple)
         {
             return new Dictionary<string, string>()
--- src/Curio.SharedKernel/Bases/Guard.cs
+++ src/Curio.SharedKernel/Bases/Guard.cs
@@ -4,8 +4,9 @@ namespace Curio.SharedKernel.Bases
 {
     public class Guard : IGuardClause, IGuardValidationClause
     {
+        // Entry points to guarding against a particular thing.
         public static IGuardClause Against { get; } = new Guard();
-        public static IGuardValidationClause AgainstV { get; } = new Guard();
+        public static IGuardValidationClause This { get; } = new Guard();
 
         private Guard()
         { }
