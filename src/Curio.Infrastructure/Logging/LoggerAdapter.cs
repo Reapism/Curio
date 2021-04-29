f3dd06d90c01211c4b375718958636ccf6e79c14
Anthony J <reapsprgm@gmail.com>
2021-04-29 00:06:25 -0400

    Add new abstraction for Logging to remove coupling of Asp.NET Logger in core project

---
+++ src/Curio.Infrastructure/Logging/LoggerAdapter.cs
@@ -0,0 +1,26 @@
+﻿using Curio.Core.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace Curio.Infrastructure.Logging
+{
+    public class LoggerAdapter<T> : IAppLogger<T>
+    {
+        private readonly ILogger<T> logger;
+
+        public LoggerAdapter(ILoggerFactory loggerFactory)
+        {
+            logger = loggerFactory.CreateLogger<T>();
+        }
+
+        public void LogWarning(string message, params object[] args)
+        {
+            logger.LogWarning(message, args);
+
+        }
+
+        public void LogInformation(string message, params object[] args)
+        {
+            logger.LogInformation(message, args);
+        }
+    }
+}
