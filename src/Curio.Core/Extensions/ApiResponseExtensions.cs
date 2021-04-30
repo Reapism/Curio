ee4cdb1f2dc13c3dc74aa65fb84324e7482a924d
Anthony J <reapsprgm@gmail.com>
2021-04-30 00:51:23 -0400

    Make static instead of extension to create instance of T and wrap it in an ApiResponse

--- src/Curio.Core/Extensions/ApiResponseExtensions.cs
+++ src/Curio.Core/Extensions/ApiResponseExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using Curio.SharedKernel;
 using Curio.SharedKernel.Interfaces;
 
@@ -46,6 +48,33 @@ namespace Curio.Core.Extensions
                 Message = message,
                 Response = hasResponse ? validationResponse : null,
                 IsSuccessful = true
+            };
+
+            return apiResponse;
+        }
+
+        /// <summary>
+        /// Return a failed <see cref="ApiResponse{T}"/> wrapped in a <see cref="IValidationResponse"/>.
+        /// <para>This modifies the <paramref name="validationResponse"/> and populates it based
+        /// on the <paramref name="validationToTipsMapping"/>.</para>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="validationResponse">The <see cref="IValidationResponse"/> instance.</param>
+        /// <param name="validationToTipsMapping">Key: Validation Message, Value: A tip if any, for why or how to fix the validation.</param>
+        /// <returns></returns>
+        public static ApiResponse<T> AsFailedApiValidationResponse<T>(IDictionary<string, string> validationToTipsMapping, string optionalMessage = "")
+            where T : class, IValidationResponse, new()
+        {
+            var validationResponse = default(T);
+
+            validationResponse.IsFailure = true;
+            validationResponse.ValidationToTipMapping = validationToTipsMapping;
+
+            var apiResponse = new ApiResponse<T>()
+            {
+                Response = validationResponse,
+                IsSuccessful = true,
+                Message = optionalMessage,
             };
 
             return apiResponse;
