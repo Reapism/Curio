2d5c4bb9f08c35292d980f91e19181be01dec2de
Anthony J <reapsprgm@gmail.com>
2021-05-01 00:27:59 -0400

    ValidationResponse now has a property to determine whether the incoming ValidationResponse is friendly.

--- src/Curio.SharedKernel/Bases/ValidationResponse.cs
+++ src/Curio.SharedKernel/Bases/ValidationResponse.cs
@@ -5,6 +5,7 @@ namespace Curio.SharedKernel.Bases
 {
     public abstract class ValidationResponse : IValidationResponse
     {
+        public bool IsFriendlyValidations { get; set; }
         public bool IsFailure { get; set; }
         public IDictionary<string, string> ValidationToTipMapping { get; set; } = new Dictionary<string, string>();
     }
