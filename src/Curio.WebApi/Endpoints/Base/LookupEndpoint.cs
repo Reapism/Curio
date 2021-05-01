cd8f38ee54ea685f0a6434c77d0f6b81927171a8
Anthony J <reapsprgm@gmail.com>
2021-05-01 00:27:12 -0400

    Add Crud/Lookup Endpoint bases.
    - These likely wont be used for now since these would make the endpoints do too much and also I am moving forward with MediatR

---
+++ src/Curio.WebApi/Endpoints/Base/LookupEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curio.Api.Endpoints.Base
+{
+    [ApiController]
+    public abstract class LookupEndpoint<TRequest, TResponse> : ControllerBase
+    {
+        [HttpGet]
+        public abstract ActionResult<TResponse> LookupAsync();
+
+        [HttpGet]
+        public abstract ActionResult<TResponse> LookupAsync(TRequest request);
+    }
+
+    [ApiController]
+    public abstract class LookupEndpointAsync<TRequest, TResponse> : ControllerBase
+    {
+        [HttpGet]
+        public abstract Task<ActionResult<TResponse>> LookupAsync(
+            CancellationToken cancellationToken = default);
+
+        [HttpGet]
+        public abstract Task<ActionResult<TResponse>> LookupAsync(
+            TRequest request,
+            CancellationToken cancellationToken = default);
+    }
+}
