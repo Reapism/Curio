3374cb42df595280dcd9f5cb1ca4dde8b9f44d87
Anthony J <reapsprgm@gmail.com>
2021-05-01 00:29:28 -0400

    Basic use of the current Endpoint builder (this is without MediatR)
    - MediatR essentially figures out which is requests or responses via classes implementing certain generic interfaces like IRequest<> and IRequestHandler<,>

---
+++ src/Curio.WebApi/Endpoints/Authentication/EndUserRegistrationEndpoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Curio.Api.Endpoints.Base;
+using Curio.WebApi.Exchanges.Home;
+using Curio.WebApi.Exchanges.Services.Home;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curio.WebApi.Endpoints.Authentication
+{
+    public class EndUserRegistrationEndpoint : EndpointBaseAsyncBuilder
+        .WithRequest<EndUserRegistrationRequest>
+        .WithResponse<RegistrationResponse>
+    {
+        private readonly IUserRegistrationService userRegistrationService;
+
+        public EndUserRegistrationEndpoint(IUserRegistrationService userRegistrationService, Mediator)
+        {
+            this.userRegistrationService = userRegistrationService;
+        }
+
+        [HttpPost]
+        public override Task<ActionResult<RegistrationResponse>> HandleAsync(EndUserRegistrationRequest request, CancellationToken cancellationToken = default)
+        {
+            
+        }
+    }
+}
