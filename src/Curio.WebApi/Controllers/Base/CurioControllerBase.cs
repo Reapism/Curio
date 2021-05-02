2ebc7d4b78dafd09ad1307bc5f08c2bfbb7b70eb
Anthony J <reapsprgm@gmail.com>
2021-05-01 23:24:08 -0400

    Create new Controller base class and removing the need of endpoints for now
    - figure out if we can use the endpoint infra with mediator without having HandleAsync for the names of the endpoints.
    - I think we can via HttpVerb attrbutes.

---
+++ src/Curio.WebApi/Controllers/Base/CurioControllerBase.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curio.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CurioControllerBase : ControllerBase
+    {
+        public CurioControllerBase(IMediator mediator)
+        {
+            Mediator = mediator;
+        }
+
+        protected IMediator Mediator { get; }
+    }
+}
