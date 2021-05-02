520d519e8beb7d0da6b155e380d5b94ecdf2048e
Anthony J <reapsprgm@gmail.com>
2021-05-01 23:24:31 -0400

    add cancellation tokan and mediator to send the request.

--- src/Curio.WebApi.Exchanges/Home/IUserRegistrationService.cs
+++ src/Curio.WebApi.Exchanges/Home/IUserRegistrationService.cs
@@ -1,11 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using Curio.SharedKernel;
 
 namespace Curio.WebApi.Exchanges.Home
 {
     public interface IUserRegistrationService
     {
-        Task<ApiResponse<RegistrationResponse>> RegisterUserAsync(EndUserRegistrationRequest registrationRequest);
+        Task<ApiResponse<RegistrationResponse>> RegisterUserAsync(EndUserRegistrationRequest registrationRequest, CancellationToken cancellationToken = default);
     }
 
 }
---
+++ src/Curio.WebApi/Controllers/EndUserRegistrationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Curio.SharedKernel;
+using Curio.WebApi.Exchanges.Home;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curio.WebApi.Controllers
+{
+    public class EndUserRegistrationController : CurioControllerBase
+    {
+        public EndUserRegistrationController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterEndUser([FromForm] EndUserRegistrationRequest endUserRegistrationRequest, CancellationToken cancellationToken = default)
+        {
+            var result = await Mediator.Send(endUserRegistrationRequest, cancellationToken);
+            return Ok(result);
+        }
+    }
+}
