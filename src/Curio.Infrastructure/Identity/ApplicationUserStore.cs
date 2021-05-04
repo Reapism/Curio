1cb07f3705b3032db8d5dab693e9da547f80ae99
Anthony J <reapsprgm@gmail.com>
2021-05-03 23:41:56 -0400

    Add new ApplicationUserStore for the identity

---
+++ src/Curio.Infrastructure/Identity/ApplicationUserStore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Curio.Infrastructure.Identity
+{
+    public class ApplicationUserStore : UserStore<ApplicationUser, ApplicationRole, CurioIdentityDbContext, Guid>
+    {
+        public ApplicationUserStore(CurioIdentityDbContext context, IdentityErrorDescriber describer = null) : base(context, describer)
+        {
+        }
+
+        public virtual Task<ApplicationUser> FindByPhoneNumberAsync(string phoneNumber, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            return Users.FirstOrDefaultAsync(u => u.PhoneNumber == phoneNumber, cancellationToken);
+        }
+    }
+}
