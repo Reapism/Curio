9680bfa65fdbc0580f8bfb64dc8791fb3dcdda1c
Anthony J <reapsprgm@gmail.com>
2021-05-08 20:04:06 -0400

    Configure to use Autofac in web api

--- src/Curio.WebApi/Program.cs
+++ src/Curio.WebApi/Program.cs
@@ -1,20 +1,67 @@
+using System;
+using Autofac.Extensions.DependencyInjection;
+using Curio.ApplicationCore.Interfaces;
+using Curio.Infrastructure.Data;
+using Curio.Infrastructure.Identity;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Curio.WebApi
 {
     public class Program
     {
+        /// <summary>Main entry point for the web api.</summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            InitializeDatabaseState(host);
+
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var hostBuilder = Host
+                .CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder
+                    .UseStartup<Startup>()
+                    .ConfigureLogging(logging =>
+                    {
+                        logging.ClearProviders();
+                        logging.AddConsole();
+                    });
+            });
+
+            return hostBuilder;
+        }
+
+        private static void InitializeDatabaseState(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    var curioClientContext = services.GetRequiredService<CurioClientDbContext>();
+                    var doesCurioClientDbExist = curioClientContext.Database.EnsureCreated();
+
+                    var curioIdentityContext = services.GetRequiredService<CurioIdentityDbContext>();
+                    var doesCurioIdentityDbExist = curioIdentityContext.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<IAppLogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+        }
     }
 }
