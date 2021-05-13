55a789c0cefad735d7eedd8a7218c634a1f7869a
Anthony J <reapsprgm@gmail.com>
2021-05-12 22:11:26 -0400

    Verify Phone number service

---
+++ src/Curio.WebApi.Exchanges/Identity/IVerifyPhoneNumberService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Curio.SharedKernel;
+using Curio.WebApi.Exchanges.Identity;
+
+namespace Curio.WebApi.Exchanges.Home
+{
+    public interface IVerifyPhoneNumberService
+    {
+        Task<ApiResponse<RegistrationResponse>> VerifyPhoneNumber(VerifyPhoneNumberRequest verifyPhoneNumberRequest, CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/Curio.WebApi.Exchanges/Identity/VerifyPhoneNumberExchange.cs
@@ -0,0 +1,14 @@
+﻿using Curio.SharedKernel.Bases;
+
+namespace Curio.WebApi.Exchanges.Identity
+{
+    public class VerifyPhoneNumberRequest
+    {
+        string PhoneNumber { get; set; }
+        bool VerifyViaMobile { get; set; }
+    }
+
+    public class VerifyPhoneNumberResponse : ValidationResponse
+    {
+    }
+}
