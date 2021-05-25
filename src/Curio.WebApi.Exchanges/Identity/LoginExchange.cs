af2aa6b8ffbe7dd3a5fb79cd48a1c9c554206ec1
Anthony J <reapsprgm@gmail.com>
2021-05-24 23:47:29 -0400

    Add country code for LoginRequest for api to validate phone number of specific country

--- src/Curio.WebApi.Exchanges/Identity/LoginExchange.cs
+++ src/Curio.WebApi.Exchanges/Identity/LoginExchange.cs
@@ -11,6 +11,9 @@ namespace Curio.WebApi.Exchanges.Identity
         // Computed values on client side whether LoginName is mobile or email
         public bool IsEmailLogin { get; set; }
         public bool IsMobileLogin { get; set; }
+        // IF using mobile, the country the number is for to help validate the phone number.
+        public string CountryCode { get; set; }
+        
     }
 
     public class LoginResponse : ValidationResponse
