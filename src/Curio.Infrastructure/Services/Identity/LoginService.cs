04eeb54652142ae2c03c62a9bf1f6b3e42f65e8f
Anthony J <reapsprgm@gmail.com>
2021-05-25 00:27:27 -0400

    LoginService
    - use new extension method

--- src/Curio.Infrastructure/Services/Identity/LoginService.cs
+++ src/Curio.Infrastructure/Services/Identity/LoginService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Curio.Core.Extensions;
 using Curio.Infrastructure.Identity;
 using Curio.SharedKernel;
 using Curio.SharedKernel.Bases;
+using Curio.SharedKernel.Interfaces;
 using Curio.WebApi.Exchanges.Identity;
 using Microsoft.AspNetCore.Identity;
 
@@ -11,46 +13,79 @@ namespace Curio.Infrastructure.Services.Identity
     public class LoginService : ILoginService
     {
         private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
 
-        public LoginService(SignInManager<ApplicationUser> signInManager)
+        public LoginService(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
         {
             this.signInManager = signInManager;
+            this.userManager = userManager;
         }
+
         public async Task<ApiResponse<LoginResponse>> LoginAsync(LoginRequest loginRequest)
         {
-
+            signInManager.
         }
 
         private async Task<ApiResponse<LoginResponse>> LoginAsyncInternal(LoginRequest loginRequest)
         {
-            Action loginAction = GetLoginAction(loginRequest);
+            var applicationUser = GetApplicationUser(loginRequest).Invoke();
+            var userExists = DoesUserExist(applicationUser);
 
-            loginAction.Invoke();
+            if (userExists)
+                LoginAsyncInternal(applicationUser);
         }
 
-        private Action GetLoginAction(LoginRequest loginRequest)
+        private async Task<ApiResponse<LoginResponse>> LoginAsyncInternal(ApplicationUser applicationUser)
+        {
+
+        }
+
+        private Func<ApplicationUser> GetApplicationUser(LoginRequest loginRequest)
         {
             return () =>
             {
+                var user = null as ApplicationUser;
                 if (loginRequest.IsEmailLogin)
-                    LoginUsingEmail(loginRequest.LoginName);
+                {
+                    ValidateEmailLogin(loginRequest.LoginName);
+                    user = GetApplicationUserFromEmail(loginRequest.LoginName);
+                }
 
                 if (loginRequest.IsMobileLogin)
-                    LoginUsingMobile(loginRequest.LoginName, loginRequest.CountryCode);
+                {
+                    ValidationMobilePhoneLogin(loginRequest.LoginName, loginRequest.CountryCode);
+                    user = GetApplicationUserFromMobilePhone(loginRequest.LoginName);
+                }
 
-                return;
+                return user;
             };
         }
 
-        private ApiResponse<LoginResponse> LoginUsingEmail(string email)
+        private bool DoesUserExist(ApplicationUser applicationUser)
         {
-            ValidationGuard.Against.Email(email, nameof(email));
+            return applicationUser is not null;
+        }
+
+        private ApplicationUser GetApplicationUserFromMobilePhone(string loginName)
+        {
+            Guard.
+        }
+
+        private ApplicationUser GetApplicationUserFromEmail(string loginName)
+        {
+            throw new NotImplementedException();
+        }
+
+        private ApiResponse<LoginResponse> ValidateEmailLogin(string email)
+        {
+            var validationResponse = ValidationGuard.Against.Email(email, nameof(email)).AsApiResponse<IValidationResponse, LoginResponse>(null, "The email is invalid.");
+
 
         }
 
-        private ApiResponse<LoginResponse> LoginUsingMobile(string mobilePhone, string countryCode)
+        private ApiResponse<LoginResponse> ValidationMobilePhoneLogin(string mobilePhone, string countryCode)
         {
-            ValidationGuard.Against.Phone(mobilePhone, countryCode, nameof(mobilePhone));
+            var validationResponse = ValidationGuard.Against.Phone(mobilePhone, countryCode, nameof(mobilePhone));
 
             mobilePhone
         }
