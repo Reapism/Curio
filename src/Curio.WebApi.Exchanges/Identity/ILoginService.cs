fe8b6f776a64ff914c94262d1a27565c86d03c25
Anthony J <reapsprgm@gmail.com>
2021-05-25 00:27:44 -0400

    ILoginService
    - Add cancellation token properly

--- src/Curio.WebApi.Exchanges/Identity/ILoginService.cs
+++ src/Curio.WebApi.Exchanges/Identity/ILoginService.cs
@@ -1,11 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using Curio.SharedKernel;
 
 namespace Curio.WebApi.Exchanges.Identity
 {
     public interface ILoginService
     {
-        Task<ApiResponse<LoginResponse>> LoginAsync(LoginRequest loginRequest);
+        Task<ApiResponse<LoginResponse>> LoginAsync(LoginRequest loginRequest, CancellationToken cancellationToken = default);
     }
 
 }
