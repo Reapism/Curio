b720b290690c7a712df0ee001d2f9d495dbc2dc6
Anthony J <reapsprgm@gmail.com>
2021-05-25 00:28:24 -0400

    LoginService
    - Add cancellation token properly in all methods
    - Remove UserManger and use ApplicationUserStore to find `ApplicationUser`(s) by email, phonenumber etc.

--- src/Curio.Infrastructure/Services/Identity/LoginService.cs
+++ src/Curio.Infrastructure/Services/Identity/LoginService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 using Curio.Core.Extensions;
 using Curio.Infrastructure.Identity;
@@ -13,48 +15,52 @@ namespace Curio.Infrastructure.Services.Identity
     public class LoginService : ILoginService
     {
         private readonly SignInManager<ApplicationUser> signInManager;
-        private readonly UserManager<ApplicationUser> userManager;
+        private readonly ApplicationUserStore applicationUserStore;
 
-        public LoginService(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        public LoginService(SignInManager<ApplicationUser> signInManager, ApplicationUserStore applicationUserStore)
         {
             this.signInManager = signInManager;
-            this.userManager = userManager;
+            this.applicationUserStore = applicationUserStore;
         }
 
-        public async Task<ApiResponse<LoginResponse>> LoginAsync(LoginRequest loginRequest)
+        public async Task<ApiResponse<LoginResponse>> LoginAsync(LoginRequest loginRequest, CancellationToken cancellationToken = default)
         {
-            signInManager.
+            return await LoginAsyncInternal(loginRequest, cancellationToken);
         }
 
-        private async Task<ApiResponse<LoginResponse>> LoginAsyncInternal(LoginRequest loginRequest)
+        private async Task<ApiResponse<LoginResponse>> LoginAsyncInternal(LoginRequest loginRequest, CancellationToken cancellationToken)
         {
-            var applicationUser = GetApplicationUser(loginRequest).Invoke();
+            var applicationUserFunction = await GetApplicationUser(loginRequest, cancellationToken);
+            var applicationUser = await applicationUserFunction.Invoke();
             var userExists = DoesUserExist(applicationUser);
 
             if (userExists)
-                LoginAsyncInternal(applicationUser);
+                return await LoginAsyncInternal(applicationUser, cancellationToken);
+
+            return GetLoginResponse();
         }
 
-        private async Task<ApiResponse<LoginResponse>> LoginAsyncInternal(ApplicationUser applicationUser)
+        private async Task<ApiResponse<LoginResponse>> LoginAsyncInternal(ApplicationUser applicationUser, CancellationToken cancellationToken)
         {
-
+            await signInManager.SignInAsync(applicationUser, true);
+            return new ApiResponse<LoginResponse>(null, (int)HttpStatusCode.OK);
         }
 
-        private Func<ApplicationUser> GetApplicationUser(LoginRequest loginRequest)
+        private async Task<Func<Task<ApplicationUser>>> GetApplicationUser(LoginRequest loginRequest, CancellationToken cancellationToken)
         {
-            return () =>
+            return async () =>
             {
                 var user = null as ApplicationUser;
                 if (loginRequest.IsEmailLogin)
                 {
                     ValidateEmailLogin(loginRequest.LoginName);
-                    user = GetApplicationUserFromEmail(loginRequest.LoginName);
+                    user = await GetApplicationUserFromEmail(loginRequest.LoginName, cancellationToken);
                 }
 
                 if (loginRequest.IsMobileLogin)
                 {
                     ValidationMobilePhoneLogin(loginRequest.LoginName, loginRequest.CountryCode);
-                    user = GetApplicationUserFromMobilePhone(loginRequest.LoginName);
+                    user = await GetApplicationUserFromMobilePhone(loginRequest.LoginName, cancellationToken);
                 }
 
                 return user;
@@ -66,28 +72,40 @@ namespace Curio.Infrastructure.Services.Identity
             return applicationUser is not null;
         }
 
-        private ApplicationUser GetApplicationUserFromMobilePhone(string loginName)
+        private async Task<ApplicationUser> GetApplicationUserFromMobilePhone(string loginName, CancellationToken cancellationToken)
         {
-            Guard.
+            return await applicationUserStore.FindByPhoneNumberAsync(loginName, cancellationToken);
         }
 
-        private ApplicationUser GetApplicationUserFromEmail(string loginName)
+        private async Task<ApplicationUser> GetApplicationUserFromEmail(string loginName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return await applicationUserStore.FindByEmailAsync(loginName, cancellationToken);
         }
 
         private ApiResponse<LoginResponse> ValidateEmailLogin(string email)
         {
-            var validationResponse = ValidationGuard.Against.Email(email, nameof(email)).AsApiResponse<IValidationResponse, LoginResponse>(null, "The email is invalid.");
+            var validationResponse = ValidationGuard
+                .Against
+                .Email(email, nameof(email))
+                .AsApiResponse<IValidationResponse, LoginResponse>(null, "The email is invalid.");
 
-
+            return validationResponse;
         }
 
         private ApiResponse<LoginResponse> ValidationMobilePhoneLogin(string mobilePhone, string countryCode)
         {
-            var validationResponse = ValidationGuard.Against.Phone(mobilePhone, countryCode, nameof(mobilePhone));
+            // need to check api response to figure out how to properly return this if its success or not. right now it assumes its invalid.
+            var validationResponse = ValidationGuard
+                .Against
+                .Phone(mobilePhone, countryCode, nameof(mobilePhone))
+                .AsApiResponse<IValidationResponse, LoginResponse>(null, $"The mobile phone provided is invalid in the specific country. Country code: {countryCode}"); ;
 
-            mobilePhone
+            return validationResponse;
+        }
+
+        private ApiResponse<LoginResponse> GetLoginResponse()
+        {
+            return new ApiResponse<LoginResponse>(null, (int)HttpStatusCode.Accepted);
         }
     }
 }
