b78a666a62a8cee3e6c546f28552907841f31e37
Anthony J <reapsprgm@gmail.com>
2021-05-24 23:46:15 -0400

    Add SessionUser

---
+++ src/Curio.Infrastructure/Identity/ISessionUser.cs
@@ -0,0 +1,24 @@
+﻿namespace Curio.Infrastructure.Identity
+{
+    /// <summary>
+    /// A basic end user stripped of sensitive account information.
+    /// </summary>
+    public interface ISessionUser
+    {
+        string Handle { get; init; }
+        byte[] ProfilePicture { get; init; }
+    }
+
+    public class SessionUser : ISessionUser
+    {
+        public SessionUser()
+            : this("Unknown", null) { }
+        public SessionUser(string handle, byte[] profilePicture)
+        {
+            Handle = handle;
+            ProfilePicture = profilePicture;
+        }
+        public string Handle { get; init; }
+        public byte[] ProfilePicture { get; init; }
+    }
+}
