de2b6bf3c7773002a5fe8f515070017e32ea58b6
Anthony J <reapsprgm@gmail.com>
2021-06-01 01:05:11 -0400

    HomeController
    - add WebControllerBase

--- src/Curio.Web/Controllers/HomeController.cs
+++ src/Curio.Web/Controllers/HomeController.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net.Http;
+using Curio.Web.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace Curio.Web.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : WebControllerBase
     {
         private readonly ILogger<HomeController> logger;
+        private readonly HttpClient httpClient;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
+            : base(httpClientFactory)
         {
             this.logger = logger;
         }
