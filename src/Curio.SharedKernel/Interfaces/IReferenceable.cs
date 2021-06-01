1bdeeda584c2073a8982b0cbbb6b9834630fd419
Anthony J <reapsprgm@gmail.com>
2021-06-01 01:04:54 -0400

    IReferenceable
    - Create IDualReferenceable and add comments

--- src/Curio.SharedKernel/Interfaces/IReferenceable.cs
+++ src/Curio.SharedKernel/Interfaces/IReferenceable.cs
@@ -2,9 +2,33 @@
 
 namespace Curio.SharedKernel.Interfaces
 {
+    /// <summary>
+    /// Represents a polymorphic domain object.
+    /// </summary>
+    /// <remarks>
+    /// Allows a domain object to be polymorpic by
+    /// specifying the name and key of the reference.
+    /// </remarks>
     public interface IReferenceable
     {
         Guid ReferenceId { get; set; }
         string ReferenceName { get; set; }
+    }
+
+    /// <summary>
+    /// Represents a dual-polymorphic domain object.
+    /// </summary>
+    /// <remarks>
+    /// Allows a domain object to be polymorpic by
+    /// specifying the name and key of the source and 
+    /// target domain references.
+    /// </remarks>
+    public interface IDualReferenceable
+    {
+        Guid TargetReferenceId { get; set; }
+        string TargetReferenceName { get; set; }
+
+        Guid SourceReferenceId { get; set; }
+        string SourceReferenceName { get; set; }
     }
 }
