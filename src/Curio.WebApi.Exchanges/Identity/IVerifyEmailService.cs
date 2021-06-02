1439aca63e3cce7780877c79d56680fcf35fb12f
Anthony J <reapsprgm@gmail.com>
2021-06-02 00:11:38 -0400

    IVerifyEmailService
    - VerifyAsync

---
+++ src/Curio.WebApi.Exchanges/Identity/IVerifyEmailService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Curio.SharedKernel;
+
+namespace Curio.WebApi.Exchanges.Identity
+{
+    public interface IVerifyEmailService
+    {
+        Task<ApiResponse<VerifyEmailResponse>> VerifyAsync(VerifyEmailRequest verifyEmailRequest, CancellationToken cancellationToken = default);
+    }
+}
