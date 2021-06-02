bfb08e0028ccf68c48d1415b7dec9e090fdb74e1
Anthony J <reapsprgm@gmail.com>
2021-06-02 00:10:03 -0400

    IMobileCodeSenderService
    - SendCodeAsync

---
+++ src/Curio.WebApi.Exchanges/Identity/IMobileCodeSenderService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Curio.SharedKernel;
+
+namespace Curio.WebApi.Exchanges.Identity
+{
+    public interface IMobileCodeSenderService
+    {
+        Task<ApiResponse<bool>> SendCodeAsync(MobileCodeSenderRequest mobileCodeSenderRequest, CancellationToken cancellationToken = default);
+    }
+}
