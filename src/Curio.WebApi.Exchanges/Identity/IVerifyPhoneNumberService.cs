b2925a43872bc94d913b8c800519a1e504270ff8
Anthony J <reapsprgm@gmail.com>
2021-06-02 00:10:39 -0400

    IVerifyPhoneNumberService
    - VerifyAsync

--- src/Curio.WebApi.Exchanges/Identity/IVerifyPhoneNumberService.cs
+++ src/Curio.WebApi.Exchanges/Identity/IVerifyPhoneNumberService.cs
@@ -1,12 +1,11 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using Curio.SharedKernel;
-using Curio.WebApi.Exchanges.Identity;
 
 namespace Curio.WebApi.Exchanges.Identity
 {
     public interface IVerifyPhoneNumberService
     {
-        Task<ApiResponse<RegistrationResponse>> VerifyPhoneNumber(VerifyPhoneNumberRequest verifyPhoneNumberRequest, CancellationToken cancellationToken = default);
+        Task<ApiResponse<VerifyPhoneNumberResponse>> VerifyAsync(VerifyPhoneNumberRequest verifyPhoneNumberRequest, CancellationToken cancellationToken = default);
     }
 }
