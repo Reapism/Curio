d4bf6d09822626f622d029ecb11a5e52653673a9
Anthony J <reapsprgm@gmail.com>
2021-06-16 00:50:41 -0400

    ApiResponseExtensions
    - TODO These need to be updated, and many more added to facilitate the creation of the HttpStatusCodes internally.

--- src/Curio.Domain/Extensions/ApiResponseExtensions.cs
+++ src/Curio.Domain/Extensions/ApiResponseExtensions.cs
@@ -8,59 +8,59 @@ namespace Curio.Core.Extensions
 {
     public static class ApiResponseExtensions
     {
-        // TODO These need to be updated, and many more added to facilate the creation of the
-        // HttpStatusCodes internally.
-        public static ApiResponse<T> AsSuccessfulApiResponse<T>(this T response, string message = "")
+        public static ApiResponse<T> AsOkApiResponse<T>(this T response, string message = "")
             where T : class
         {
-            bool hasResponse = response.Equals(default(T));
-
-            var apiResponse = new ApiResponse<T>(message, httpStatusCode: 200)
-            {
-                Response = hasResponse ? response : null,
-                IsSuccessful = true,
-            };
-
-            return apiResponse;
+            return AsApiResponse(response, message, httpStatusCode: 200);
         }
 
-        public static ApiResponse<T> AsFailedApiResponse<T>(this T response, string message = "", Exception ex = null)
+        public static ApiResponse<T> AsBadRequestApiResponse<T>(this T response, string message = "")
             where T : class
         {
-            bool hasResponse = response.Equals(default(T));
+            return AsApiResponse(response, message, httpStatusCode: 400);
+        }
 
-            var apiResponse = new ApiResponse<T>(message, ex, 400)
-            {
-                Response = hasResponse ? response : null,
-                IsSuccessful = false,
-            };
+        public static ApiResponse<T> AsUnauthorizedApiResponse<T>(this T response, string message = "", Exception exception = null)
+            where T : class
+        {
+            return AsApiResponse(response, message, httpStatusCode: 401);
+        }
 
-            return apiResponse;
+        public static ApiResponse<T> AsForbidApiResponse<T>(this T response, string message = "")
+            where T : class
+        {
+            return AsApiResponse(response, message, httpStatusCode: 403);
+        }
+
+        public static ApiResponse<T> AsNotFoundApiResponse<T>(this T response, string message = "")
+            where T : class
+        {
+            return AsApiResponse(response, message, httpStatusCode: 404);
         }
 
         public static ApiResponse<T> AsApiResponse<T>(this T validationResponse, string message = "", Exception ex = null)
             where T : class, IValidationResponse
         {
-            // If validation response is not default.
-            bool hasResponse = !(bool)(validationResponse?.Equals(default(T)));
+            // If validation response is not default (empty).
+            bool hasValidationResponse = HasValidationResponse(validationResponse);
             var apiResponse = new ApiResponse<T>(message, ex, 400)
             {
-                Response = hasResponse ? validationResponse : null,
-                IsSuccessful = false
+                Response = hasValidationResponse ? validationResponse : null,
+                IsSuccessful = IsSuccessfulForValidationResponse(hasValidationResponse)
             };
 
             return apiResponse;
         }
 
-        public static ApiResponse<T> AsApiResponse<T>(this T validationResponse, ApiResponse fromApiResponse)
+        public static ApiResponse<T> FromApiResponse<T>(this T validationResponse, ApiResponse fromApiResponse)
             where T : class, IValidationResponse
         {
             // If validation response is not default.
-            bool hasResponse = !(bool)(validationResponse?.Equals(default(T)));
+            bool hasValidationResponse = HasValidationResponse(validationResponse);
             var apiResponse = new ApiResponse<T>(fromApiResponse.Message, null, 400)
             {
-                Response = hasResponse ? validationResponse : null,
-                IsSuccessful = fromApiResponse.IsSuccessful
+                Response = hasValidationResponse ? validationResponse : null,
+                IsSuccessful = IsSuccessfulForValidationResponse(hasValidationResponse)
             };
 
             return apiResponse;
@@ -81,13 +81,12 @@ namespace Curio.Core.Extensions
             where V : class, IValidationResponse
         {
             // If validation response is not default.
-            bool hasResponse = !(bool)(validationResponse?.Equals(default(T)));
-            var value = default(V);
+            bool hasValidationResponse = HasValidationResponse(validationResponse);
+            var response = default(V);
             var apiResponse = new ApiResponse<V>(optionalMessage, ex, 400)
             {
-                Response = value,
-                IsSuccessful = false,
-                
+                Response = response,
+                IsSuccessful = IsSuccessfulForValidationResponse(hasValidationResponse),
             };
 
             return apiResponse;
@@ -96,29 +95,74 @@ namespace Curio.Core.Extensions
         /// <summary>
         /// Return a failed <see cref="ApiResponse{T}"/> wrapped in a <see cref="IValidationResponse"/>.
         /// <para>This modifies the <paramref name="validationResponse"/> and populates it based
-        /// on the <paramref name="validationToTipsMapping"/>.</para>
+        /// on the <paramref name="reasonsByErrorMapping"/>.</para>
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="validationResponse">The <see cref="IValidationResponse"/> instance.</param>
-        /// <param name="validationToTipsMapping">Key: Validation Message, Value: A tip if any, for why or how to fix the validation.</param>
+        /// <param name="reasonsByErrorMapping">Key: Error Value: Friendly validation message on why the error occured.</param>
         /// <returns></returns>
-        public static ApiResponse<T> AsApiResponse<T>(IDictionary<string, string> validationToTipsMapping, string optionalMessage = "")
+        public static ApiResponse<T> AsApiResponse<T>(this IDictionary<string, string> reasonsByErrorMapping, string optionalMessage = "")
             where T : class, IValidationResponse, new()
         {
+            var hasValidations = reasonsByErrorMapping?.Any() ?? false;
+
             var validationResponse = default(T);
+            validationResponse.ReasonByErrorMapping = reasonsByErrorMapping;
 
-            var hasValidations = validationToTipsMapping?.Any() ?? false;
+            return AsApiResponse(validationResponse: validationResponse, message: optionalMessage);
+        }
 
-            validationResponse.IsFailure = hasValidations;
-            validationResponse.FriendlyValidationMapping = validationToTipsMapping;
+        private static ApiResponse<T> AsApiResponse<T>(this T response, string message = "", Exception exception = null, int httpStatusCode = 200)
+            where T : class
+        {
+            var hasResponse = HasResponse(response);
 
-            var apiResponse = new ApiResponse<T>(optionalMessage, httpStatusCode: 400)
+            var apiResponse = new ApiResponse<T>(message, exception, httpStatusCode)
             {
-                Response = validationResponse,
-                IsSuccessful = !validationResponse.IsFailure
+                IsSuccessful = IsSuccessStatusCode(httpStatusCode),
+                Response = response
             };
 
             return apiResponse;
+        }
+
+        private static bool IsSuccessStatusCode(int httpStatusCode)
+        {
+            return (httpStatusCode >= 200) && (httpStatusCode <= 299);
+        }
+
+        // public for unit testing or use the other public functions to test.
+        private static bool HasResponse<T>(this T response)
+            where T : class
+        {
+            var hasResponse = !response?.Equals(default(T)) ?? false;
+
+            return hasResponse;
+        }
+
+        private static bool HasValidationResponse<T>(this T validationResponse)
+            where T : class, IValidationResponse
+        {
+            if (validationResponse is null)
+                return false;
+
+            if (validationResponse.IsFailure)
+                return true;
+
+            if (validationResponse.ReasonByErrorMapping.Any())
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// If there is a validation response, the response is unsuccessful as there is a validation. 
+        /// </summary>
+        /// <param name="hasResponse"></param>
+        /// <returns></returns>
+        private static bool IsSuccessfulForValidationResponse(bool hasValidationResponse)
+        {
+            return !hasValidationResponse;
         }
     }
 }
