f97ebaed1b3bd001ccb6ae3059abb9186af5c61c
Anthony J <reapsprgm@gmail.com>
2021-06-17 00:13:56 -0400

    ExchangeTests
    - Add two tests
    - Learned that you cannot override the fact that default(T) where T : class is always null

---
+++ tests/Curio.UnitTests/WebApi.Exchanges/ExchangesTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Curio.WebApi.Exchanges.Identity;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Curio.UnitTests.WebApi.Exchanges
+{
+    [TestFixture]
+    public class ExchangesTests
+    {
+        [Test]
+        public void ValidationResponseIsFullyInitialized()
+        {
+            var loginResponse = new LoginResponse();
+            loginResponse.ReasonByErrorMapping.Should().NotBeNull().And.BeEmpty();
+        }
+
+        [Test]
+        public void ValidationResponseByDefaultIsNull()
+        {
+            var loginResponse = default(LoginResponse);
+            loginResponse.ReasonByErrorMapping.Should().BeNull();
+        }
+    }
+}
