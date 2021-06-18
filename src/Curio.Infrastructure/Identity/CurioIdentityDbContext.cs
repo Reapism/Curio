775499e1af22edad1aafd9a5de42bf8cde87d30e
Anthony J <reapsprgm@gmail.com>
2021-06-18 00:40:31 -0400

    CurioIdentityDbContext
    - Add required base.OnModelCreating(builder);
    - This removed the error "The entity type 'IdentityUserLogin<Guid>' requires a primary key to be defined. If you intended to use a keyless entity type, call 'HasNoKey' in 'OnModelCreating'. For more information on keyless entity types, see https://go.microsoft.com/fwlink/?linkid=21..."

--- src/Curio.Infrastructure/Identity/CurioIdentityDbContext.cs
+++ src/Curio.Infrastructure/Identity/CurioIdentityDbContext.cs
@@ -16,6 +16,7 @@ namespace Curio.Infrastructure.Identity
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
+            base.OnModelCreating(builder);
             builder.ApplyConfigurationsFromAssembly(typeof(CurioIdentityDbContext).Assembly);
         }
     }
