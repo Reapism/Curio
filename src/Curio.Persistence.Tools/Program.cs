54ff9fc6aa8111657dfab7829f9a0a8841be1a94
Anthony J <reapsprgm@gmail.com>
2021-06-19 00:55:02 -0400

    Refactor Infrastructure project to seperate persistence layer (commit series)
    2. Create Tools Project for Postrge provider

---
+++ src/Curio.Persistence.Tools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Curio.Persistence.Tools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The entry point for running Entity Framework Core Tools.");
+        }
+    }
+}
