ba0f254f1ae0954da4b0dce451c75556c9ddb308
Anthony J <reapsprgm@gmail.com>
2021-06-25 00:45:12 -0400

    Contructor injection works fine via page model ctor and @inject on razor page

--- src/Curio.Web/Pages/About/Index.cshtml.cs
+++ src/Curio.Web/Pages/About/Index.cshtml.cs
@@ -1,9 +1,18 @@
+using Curio.Core.Entities;
+using Curio.SharedKernel.Interfaces;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Curio.Web.Pages.About
 {
     public class IndexModel : PageModel
     {
+        public IndexModel(IRepository<UserPost> userPostRepository)
+        {
+            UserPostRepository = userPostRepository;
+        }
+
+        public IRepository<UserPost> UserPostRepository { get; }
+
         public void OnGet()
         {
         }
