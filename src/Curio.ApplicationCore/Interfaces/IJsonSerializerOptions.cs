ae04bcab6eac4714ed46a10f88a48b28a73b08ac
Anthony J <reapsprgm@gmail.com>
2021-07-15 16:16:51 -0400

    IJsonSerializerOptions and implementation

---
+++ src/Curio.ApplicationCore/Interfaces/IJsonSerializerOptions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Curio.ApplicationCore.Interfaces
+{
+    public interface IJsonSerializerOptions<TOptions>
+    {
+        TOptions Options { get; }
+    }
+
+    public class JsonSerializerOptionsWrapper : IJsonSerializerOptions<JsonSerializerOptions>
+    {
+        private JsonSerializerOptions jsonSerializerOptions;
+
+        public JsonSerializerOptionsWrapper()
+            : this(null)
+        { }
+
+        public JsonSerializerOptionsWrapper(JsonSerializerOptions jsonSerializerOptions)
+        {
+            this.jsonSerializerOptions = jsonSerializerOptions;
+        }
+
+        public JsonSerializerOptions Options
+        {
+            get
+            {
+                if (jsonSerializerOptions is null)
+                    jsonSerializerOptions = GetDefaultOptions();
+
+                return jsonSerializerOptions;
+            }
+            set
+            {
+                if (value is null)
+                    throw new ApplicationException("Cannot set the options to be null.");
+
+                jsonSerializerOptions = value;
+            }
+        }
+
+        public static JsonSerializerOptions GetDefaultOptions()
+        {
+            var jsonSerializerOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web)
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            return jsonSerializerOptions;
+        }
+    }
+}
--- src/Curio.ApplicationCore/Interfaces/ISerializerOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json;
-
-namespace Curio.ApplicationCore.Interfaces
-{
-    public interface ISerializerOptions
-    {
-        JsonSerializerOptions JsonOptions { get; }
-    }
-}
