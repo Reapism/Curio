8d8008510563b5459d8978d5014640c97e08a65f
Anthony J <reapsprgm@gmail.com>
2021-07-22 00:35:29 -0400

    Quick test to verify creating the path.

---
+++ tests/Curio.UnitTests/GetPath.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Reflection;
+using Xunit;
+
+namespace Curio.UnitTests
+{
+    public class GetPath
+    {
+        [Fact]
+        public void GetWebApiPathTest()
+        {
+            var path = GetWebApiPath();
+            var c = 0;
+        }
+
+        public static string GetWebApiPath()
+        {
+            var webApiProjectName = "Curio.WebApi";
+            var srcDirectoryName = "src";
+            var thisAssemblyPath = new DirectoryInfo(Assembly.GetExecutingAssembly().Location);
+            var srcPath = thisAssemblyPath.Parent.Parent.Parent.Parent.Parent.Parent.FullName;
+            var webApiPath = Path.Combine(srcPath, srcDirectoryName, webApiProjectName);
+
+            return webApiPath;
+        }
+    }
+
+}
